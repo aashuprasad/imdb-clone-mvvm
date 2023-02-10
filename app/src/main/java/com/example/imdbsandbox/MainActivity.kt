39c9567b2d9ae6db4f9c7db883b8d0409a8781f9
Ashu Prasad <aashuprasad@live.com>
2023-02-10 19:10:34 +0530

    viewmodel setup for main activity[40/100]

--- app/src/main/java/com/example/imdbsandbox/MainActivity.kt
+++ app/src/main/java/com/example/imdbsandbox/MainActivity.kt
@@ -2,16 +2,24 @@ package com.example.imdbsandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.imdbsandbox.databinding.ActivityMainBinding
+import com.example.imdbsandbox.network.APIService
+import com.example.imdbsandbox.network.APIUtility
 import com.example.imdbsandbox.network.models.Movie
+import com.example.imdbsandbox.viewmodels.MainViewModel
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
 
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 }
--- app/src/main/java/com/example/imdbsandbox/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/imdbsandbox/viewmodels/MainViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.imdbsandbox.viewmodels
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.imdbsandbox.network.APIService
+import com.example.imdbsandbox.network.APIUtility
+import com.example.imdbsandbox.network.models.Movie
+
+enum class MovieAPIStatus{LOADING, ERROR, DONE}
+
+class MainViewModel : ViewModel(){
+    val movieAPI = APIUtility.getInstance().create(APIService::class.java)
+
+    private var movieDataList: ArrayList<Movie> = ArrayList()
+
+
+    private fun getMovieData() {
+        /*viewModelScope.launch {
+            _status.value = MovieAPIStatus.LOADING
+            try {
+                _status.value = MovieAPIStatus.DONE
+                _properties.value = MarsApi.retrofitService.()
+            }catch (e:Exception){
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()}
+        }*/
+    }
 }

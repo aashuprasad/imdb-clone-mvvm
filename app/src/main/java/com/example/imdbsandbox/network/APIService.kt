de93684186621fe23ce1824dddb4de3839b7f4fa
edgynium <aashuprasad603@gmail.com>
2023-02-13 08:00:57 +0530

    feb 12

--- app/src/main/java/com/example/imdbsandbox/network/APIService.kt
+++ app/src/main/java/com/example/imdbsandbox/network/APIService.kt
@@ -2,11 +2,24 @@ package com.example.imdbsandbox.network
 
 import com.example.imdbsandbox.network.models.Movie
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 const val BASE_URL = "https://raw.githubusercontent.com/"
 
+private val retrofit =Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
 interface APIService{
     @GET("/theapache64/top250/master/top250.json")
     suspend fun getData(): Response<Movie>
+}
+
+object MovieAPI{
+    val retrofitService : APIService by lazy {
+        retrofit.create(APIService::class.java)
+    }
 }
--- app/src/main/java/com/example/imdbsandbox/network/APIUtilities.kt
+++ app/src/main/java/com/example/imdbsandbox/network/APIUtilities.kt
@@ -3,8 +3,8 @@ package com.example.imdbsandbox.network
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object APIUtility {
-    fun getInstance(): Retrofit {
+object APIUtilities {
+    fun getInstance():Retrofit{
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())

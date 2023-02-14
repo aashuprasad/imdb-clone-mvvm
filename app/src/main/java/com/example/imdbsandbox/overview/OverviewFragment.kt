45ca8dd6bd5f23bd7c25f64fcf7a21869af6d16c
Ashu Prasad <aashuprasad@live.com>
2023-02-14 12:53:56 +0530

    loaded and displayed image with glide

--- app/src/main/java/com/example/imdbsandbox/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/imdbsandbox/overview/OverviewFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.imdbsandbox.R
 import com.example.imdbsandbox.databinding.FragmentOverviewBinding
+import com.example.imdbsandbox.databinding.GridViewItemBinding
 
 class OverviewFragment : Fragment() {
 
@@ -22,7 +23,7 @@ class OverviewFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = FragmentOverviewBinding.inflate(inflater)
+        val binding = GridViewItemBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
